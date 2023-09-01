739479c1ea46880a52045040fffe161d59d66329
Giercko Contreras <136128236+gierckocontrerassixbell@users.noreply.github.com>
2023-09-01 15:04:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000; // Puerto en el que se ejecutará la aplicación
 
-app.use(express.static('public'))
+// Middleware para parsear JSON
+app.use(express.json());
 
+// Ruta de ejemplo
 app.post('/echo', (req, res) => {
-    console.log(req);
-    res.json(req)
-})
+  res.json({ message: '¡Hola, mundo!' });
+});
 
+// Manejador de errores para rutas no encontradas
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Ruta no encontrada' });
+});
+
+// Manejador de errores global
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Algo salió mal' });
+});
+
+// Iniciar el servidor
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`La aplicación está escuchando en el puerto ${port}`);
+});
